ee5c033cf41bcb371e18178119362de1a5388517
nguyen8tn <nguyen8tn@gmail.com>
2020-10-01 21:26:55 +0700

    First commit

---
+++ Request/WorkShiftRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAW_WebApi.Request
+{
+    public class WorkShiftRequest : IdRequest
+    {
+        public DateTime createTime { get; set; }
+        public string content { get; set; }
+        public string nameWifi { get; set; }
+
+    }
+}
